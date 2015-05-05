b3857b6e22515fa6b7ce37655805255a5dadc34a
pangratz <cmueller.418@gmail.com>
2015-05-06 00:04:00 +0200

    Add test for multiline string template

--- tests/unit/views/precompile-test.js
+++ tests/unit/views/precompile-test.js
@@ -22,3 +22,18 @@ test('using `hbs` tagged string places the precompiled template', function(asser
 
   assert.equal(view.$().html().trim(), "greeting: hello from view", "inline precompile of the HTMLBars template works");
 });
+
+test('multiline string', function(assert) {
+  var view = Ember.View.create({
+    greeting: "hello from view",
+    template: hbs`
+      greeting: <span>{{view.greeting}}</span>
+    `
+  });
+
+  Ember.run(function() {
+    view.appendTo('#ember-testing');
+  });
+
+  assert.equal(view.$().html().trim(), "greeting: <span>hello from view</span>", "multiline string works");
+});
