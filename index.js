689a598df9d4e2a71daef2ee7e3d25103646ca86
pangratz <cmueller.418@gmail.com>
2015-05-05 20:39:15 +0200

    Add babel transform to replace tagged string with precompiled template
    By importing the default export of the virtual module
    `htmlbars-inline-precompile`, a 'helper' is available which replaces a
    tagged string with the precompiled template of the string. This is
    useful in tests which need a compiled template, like in a component test
    for exapmple:
    
    ``` js
    // tests/unit/components/x-bar-test.js
    import precompile from 'htmlbars-inline-precompile';
    import { moduleForComponent, test } from 'ember-qunit';
    
    moduleForComponent('x-bar');
    
    test('value is available in template', function(assert) {
      var component = this.subject({
        leValue: "hello precompilation",
    
        template: precompile`leValue={{leValue}}`
      });
    
      this.render();
      assert.equal(component.$().html().trim(), "leValue=hello precompilation");
    });
    ```

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var HTMLBarsInlinePrecompilePlugin = require('./lib/babel-transform-tagged-template');
+
 module.exports = {
-  name: 'ember-cli-htmlbars-inline-precompile'
+  name: 'ember-cli-htmlbars-inline-precompile',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.options.babel = app.options.babel || {};
+    app.options.babel.plugins = app.options.babel.plugins || [];
+
+    var Compiler = require(this.templateCompilerPath());
+    var PrecompileInlineHTMLBarsPlugin = HTMLBarsInlinePrecompilePlugin(Compiler);
+
+    // add the HTMLBarsInlinePrecompilePlugin to the list of plugins used by
+    // the `ember-cli-babel` addon
+    app.options.babel.plugins.push(PrecompileInlineHTMLBarsPlugin);
+  },
+
+  // borrowed from ember-cli-htmlbars http://git.io/vJDrW
+  projectConfig: function () {
+    return this.project.config(process.env.EMBER_ENV);
+  },
+
+  // borrowed from ember-cli-htmlbars http://git.io/vJDrw
+  templateCompilerPath: function() {
+    var config = this.projectConfig();
+    var templateCompilerPath = config['ember-cli-htmlbars'] && config['ember-cli-htmlbars'].templateCompilerPath;
+
+    if (!templateCompilerPath) {
+      templateCompilerPath = this.project.bowerDirectory + '/ember/ember-template-compiler';
+    }
+
+    return path.resolve(this.project.root, templateCompilerPath);
+  }
 };
