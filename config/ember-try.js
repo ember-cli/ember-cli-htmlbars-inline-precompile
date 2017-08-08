8492e976b9e71e8e1bb5111569102ce7c82bcb02
Robert Jackson <rjackson@linkedin.com>
2017-08-08 10:54:00 -0400

    Update CI to do multiple builds in the same CI run.
    The prior configuration masked caching issues by always running
    separate Ember versions in separate processes (without a shared
    persistent cache).
    
    This migrates to using `ember try:each` (instead of many CI jobs
    with `ember try:one`) to ensure that we are properly testing the
    cache invalidation logic.

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,28 +3,6 @@ module.exports = {
   command: 'ember test',
   scenarios: [
     {
-      name: 'ember-1.13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
-        }
-      }
-    },
-    {
-      name: 'ember-lts-2.4',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-4'
-        },
-        resolutions: {
-          'ember': 'lts-2-4'
-        }
-      }
-    },
-    {
       name: 'ember-lts-2.8',
       bower: {
         dependencies: {
@@ -32,6 +10,21 @@ module.exports = {
         },
         resolutions: {
           'ember': 'lts-2-8'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.12',
+      bower: {
+        dependencies: {
+          'ember': null,
+          'ember-cli-shims': null
+        }
+      },
+      npm: {
+        dependencies: {
+          'ember-source': '~2.12.0',
+          'ember-cli-shims': '^1.0.0'
         }
       }
     },
@@ -58,6 +51,7 @@ module.exports = {
       }
     },
     {
+      allowedToFail: true,
       name: 'ember-canary',
       bower: {
         dependencies: {
