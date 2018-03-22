f33b729678f0fdecc7442c820d70f986577e9849
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-03-22 14:46:19 +0100

    Add support for importing from `ember-cli-htmlbars-inline-precompile`

--- tests/unit/components/inline-precompilation-test.js
+++ tests/unit/components/inline-precompilation-test.js
@@ -1,4 +1,5 @@
 import hbs from 'htmlbars-inline-precompile';
+import hbs2 from 'ember-cli-htmlbars-inline-precompile';
 import { moduleForComponent, test } from 'ember-qunit';
 
 moduleForComponent('inline-precompilation', {
@@ -7,6 +8,12 @@ moduleForComponent('inline-precompilation', {
 
 test('using `hbs` tagged string places the precompiled template', function(assert) {
   this.render(hbs`{{inline-precompilation}}`);
+
+  assert.equal(this.$().text().trim(), "greeting: hello from view", "inline precompile of the HTMLBars template works");
+});
+
+test('using `hbs` tagged string  from `ember-cli-htmlbars-inline-precompile` replaces the precompiled template', function(assert) {
+  this.render(hbs2`{{inline-precompilation}}`);
 
   assert.equal(this.$().text().trim(), "greeting: hello from view", "inline precompile of the HTMLBars template works");
 });
